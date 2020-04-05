06e12c3667dba66209aeb0eb906d2ae829c20e44
ashad12 <a.shademani@gmail.com>
2020-04-05 10:33:29 -0700

    feat: model definition

--- pneumonia.py
+++ pneumonia.py
@@ -10,8 +10,9 @@ import numpy as np
 import tqdm
 from torchvision import transforms
 from torchvision import datasets
+import torch.nn as nn
 
-
+##Preprocessing and loading data
 transform = transforms.Compose([transforms.Resize(800), transforms.CenterCrop(500),
                                 transforms.Resize(224), transforms.RandomRotation(10), transforms.ToTensor(),
                                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -24,3 +25,19 @@ test = datasets.ImageFolder(os.path.join(dir, 'test'), transform=transform)
 train_loader = torch.utils.data.DataLoader(train, batch_size=64, shuffle=True)
 val_loader = torch.utils.data.DataLoader(train, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(train, batch_size=64, shuffle=True)
+
+##Defining the model
+for param in detector.parameters():
+  param.requires_grad = False
+
+for p in detector.fc.parameters():
+  p.requires_grad = True
+
+classifier = nn.Sequential(nn.Linear(1000, 500),
+                           nn.ReLU(),
+                           nn.Dropout(.3),
+                           nn.Linear(500, 200),
+                           nn.ReLU(), nn.Dropout(.3),
+                           nn.Linear(200, 30), nn.ReLU(), nn.Dropout(.3),
+                           nn.Linear(30, 1))
+detector.add_module('classifier',classifier)
